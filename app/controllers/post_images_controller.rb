86d1f030239c75a4ad18599aec7eb9eb9a38363c
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-07-09 00:38:19 +0000

    [update]mesiterro-20-finish

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -17,9 +17,10 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
-def destroy
+  def destroy
     post_image = PostImage.find(params[:id])
     post_image. destroy
     redirect_to '/post_images'
