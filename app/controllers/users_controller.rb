8be0154dd896069105c2f7f0dd862b13e278f760
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-07-10 04:06:14 +0000

    [update]meshiterro-26-?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
 	  @user = User.find(params[:id])
 	  @post_images = @user.post_images.page(params[:page])
 	end
 
   def edit
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def update
@@ -18,5 +19,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  # ログインしているユーザーのidとURLに含まれるidを比較し、一致しなければ投稿画像一覧ページに移動する
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
